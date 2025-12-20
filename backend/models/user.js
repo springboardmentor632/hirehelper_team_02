aeb20b4595d33deff95156353d8a0d4423afb641
Bijay Kund <your_github_email@gmail.com>
2025-12-20 15:59:53 +0530

    Restore user model after merge

---
+++ backend/models/user.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phoneNumber: String,
+    isVerified: { type: Boolean, default: false },
+    otp: String,
+    otpExpires: Date
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
